4586bcff20368215c442b214ee1d11e7d6af5e2a
Natarajan <natarajanspnk@gmail.com>
2025-11-12 08:10:04 +0000

    Try fixing this error: `Runtime Error: Error: servers is not defined. Er

--- src/app/consultation/[id]/page.tsx
+++ src/app/consultation/[id]/page.tsx
@@ -63,6 +63,16 @@ type CallData = {
     doctorName?: string;
 }
 
+const servers = {
+  iceServers: [
+    {
+      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
+    },
+  ],
+  iceCandidatePoolSize: 10,
+};
+
+
 export default function ConsultationPage({
   params,
 }: {
