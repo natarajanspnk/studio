972a381ea568f5121498094e5504cbf4ea3494c1
natarajanspnk <natarajanspnk@gmail.com>
2025-11-14 16:03:19 +0530

    fixed index file error

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,7 +9,7 @@
 
 import {setGlobalOptions} from "firebase-functions";
 import {onRequest} from "firebase-functions/https";
-import {logger} from "firebase-functions/logger";
+import * as logger from "firebase-functions/logger";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -26,7 +26,7 @@ import {logger} from "firebase-functions/logger";
 // this will be the maximum concurrent request count.
 setGlobalOptions({ maxInstances: 10 });
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = onRequest((request, response) => {
+   logger.info("Hello logs!", {structuredData: true});
+   response.send("Hello from Firebase!");
+});
